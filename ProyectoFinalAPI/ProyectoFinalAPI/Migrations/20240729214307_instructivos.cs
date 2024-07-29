832e678f110aca5f443579a95dc1e795edde4536
IGDS801-20001472 <mazu1234567@outlook.com>
2024-07-29 15:45:49 -0600

    Adicion del modelo de instructivos

---
+++ ProyectoFinalAPI/ProyectoFinalAPI/Migrations/20240729214307_instructivos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class instructivos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdProducto",
+                table: "Producto",
+                newName: "idProducto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "idProducto",
+                table: "Producto",
+                newName: "IdProducto");
+        }
+    }
+}
