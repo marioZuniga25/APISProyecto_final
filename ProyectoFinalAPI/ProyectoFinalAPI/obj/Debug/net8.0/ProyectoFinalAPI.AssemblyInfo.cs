7e97310dfec75a2988e9d6342eebd23c0c34db5d
Cristian Alejandro Leyva Collazo <cristianleyvacr7@gmail.com>
2024-08-09 14:49:10 -0600

    cambio

--- ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1640fef15a373428de863fd815a4fdf1dfb9a17f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f535db1487d33863cd85cbdb31ca975d014acfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
