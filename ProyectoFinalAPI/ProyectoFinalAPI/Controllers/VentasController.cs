05fddf455069d6bc5b5223d3fdbd6896708da0f9
IGDS801-20001472 <mazu1234567@outlook.com>
2024-08-14 20:15:06 -0600

    Rebajar stock de productos

--- ProyectoFinalAPI/ProyectoFinalAPI/Controllers/VentasController.cs
+++ ProyectoFinalAPI/ProyectoFinalAPI/Controllers/VentasController.cs
@@ -36,16 +36,34 @@ namespace ProyectoFinalAPI.Controllers
         [HttpPost("AgregarDetalleVenta")]
         public async Task<ActionResult> AddDetallesVenta([FromBody] List<DetalleVenta> detallesVenta)
         {
-            if (detallesVenta == null || !detallesVenta.Any())
-            {
-                return BadRequest("La lista de detalles de venta no puede estar vacía.");
-            }
+            
+                foreach (var detalle in detallesVenta)
+                {
+                    // Buscar el producto
+                    var producto = await _context.Producto
+                        .FirstOrDefaultAsync(p => p.idProducto == detalle.idProducto);
 
-            // Agregar lógica para guardar los detalles de venta
-            foreach (var detalle in detallesVenta)
-            {
-                _context.DetalleVenta.Add(detalle);
-            }
+                    if (producto == null)
+                    {
+                        return NotFound($"El producto con ID {detalle.idProducto} no se encontró.");
+                    }
+
+                    
+                    if (producto.stock < detalle.cantidad)
+                    {
+                        return BadRequest($"No hay suficiente stock del producto '{producto.nombreProducto}'. Stock disponible: {producto.stock}");
+                    }
+
+                    
+                    producto.stock -= detalle.cantidad;
+
+                    
+                    _context.DetalleVenta.Add(detalle);
+                }
+
+                
+                
+
 
             await _context.SaveChangesAsync();
 
