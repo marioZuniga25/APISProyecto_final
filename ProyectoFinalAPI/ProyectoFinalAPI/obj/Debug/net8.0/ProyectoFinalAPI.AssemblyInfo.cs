f09d7f95b31739fe3f11368c4ffbb9070cfecb6b
IDGS80121000466 <21000466@alumnos.utleon.edu.mx>
2024-08-16 02:35:42 -0600

    Flutter terminada y api que regresa los productos por idCategoria

--- ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3253b1aad69dbe12189ac0932791587ee64da8dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8852149e92b287aae83fed2be13291bab6e6d080")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
