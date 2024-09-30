84d8f66759eb4603e5253de7a1996e4d73704f81
Ángel Martínez <angelroberto7777@gmail.com>
2024-09-30 17:50:39 -0600

    prueba

--- ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b060614b3efa89856fdd15e7cdbdc0458eea58c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15f947ccefd7c32bbeb0176dc91f17cbdd4479a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
