ee0682ee5138a543fab929b97ff518dc10894869
Cristian Alejandro Leyva Collazo <cristianleyvacr7@gmail.com>
2024-11-14 15:48:04 -0600

    subiendo skin

--- ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3403d4b04eacb904d887f6706832c5c6cc6b938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddfcb7a5d59db4d3dafa2cfc4f3e2caa3e1b188")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
