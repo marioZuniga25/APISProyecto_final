379f5f08cf4654839692ea3abcea316ec6af7974
Cristian Alejandro Leyva Collazo <cristianleyvacr7@gmail.com>
2024-11-14 15:52:41 -0600

    subiendo pyhton

--- ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddfcb7a5d59db4d3dafa2cfc4f3e2caa3e1b188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd82dc4e7f1490fcb808247b775b860bb1fb1f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
