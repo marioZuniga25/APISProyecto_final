47bb5df043fa997a2b831523e28e7f72d48281fc
IDGS80121000466 <21000466@alumnos.utleon.edu.mx>
2024-11-26 15:32:03 -0600

    Guardando cambios antes de hacer el merge con main

--- ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698aef1ee1217ecc047308c890b02bacc4ec537c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0399d4b17d7053d9c0a4f0bc51e42ed3215431")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
