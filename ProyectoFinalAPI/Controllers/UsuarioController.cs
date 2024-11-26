3df118db7cc0123adfb787581fe6c2725fb0e579
PedroVariable <110512432+PedroVariable@users.noreply.github.com>
2024-11-26 09:07:23 -0600

    Update UsuarioController.cs

--- ProyectoFinalAPI/Controllers/UsuarioController.cs
+++ ProyectoFinalAPI/Controllers/UsuarioController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoFinalAPI.Models;
 using System.Threading;
@@ -145,14 +145,11 @@ namespace ProyectoFinalAPI.Controllers
   }
 
 
-        [HttpPost]
-        [Route("registrar")]
-        public async Task<IActionResult> AddUsuario([FromBody] Usuario request, [FromServices] EmailService emailService)
-        {
-            if (await IsPasswordUnsafe(request.contrasenia))
-            {
-                return BadRequest(new { message = "La contraseña ingresada es insegura." });
-            }
+  [HttpDelete]
+  [Route("EliminarUsuario/{id:int}")]
+  public async Task<IActionResult> deleteUsuario(int id)
+  {
+   var usuarioEliminar = await _context.Usuario.FindAsync(id);
 
    if (usuarioEliminar == null)
    {
@@ -162,18 +159,7 @@ namespace ProyectoFinalAPI.Controllers
 
    await _context.SaveChangesAsync();
 
-            await _context.Usuario.AddAsync(usuario);
-            await _context.SaveChangesAsync();
-
-            // Preparar el correo con el template de bienvenida
-            var body = System.IO.File.ReadAllText("Templates/WelcomeEmail.html")
-                       .Replace("[LOGO_URL]", "https://i.imgur.com/EmvHFiH.png")
-                       .Replace("[NOMBRE_USUARIO]", usuario.nombreUsuario);
-
-            await emailService.SendEmailAsync(usuario.correo, "¡Bienvenido a nuestra plataforma!", body);
-
-            return Ok(request);
-        }
+   return Ok();
 
   }
 
@@ -415,264 +401,39 @@ namespace ProyectoFinalAPI.Controllers
    var usuario = await _context.Usuario.FirstOrDefaultAsync(
        u => u.ResetToken == request.Token && u.ResetTokenExpires > DateTime.UtcNow);
 
-        [HttpPost]
-        [Route("Login")]
-        public async Task<IActionResult> Login([FromBody] Usuario request, [FromServices] EmailService emailService)
-        {
-            var usuario = await _context.Usuario
-                .Include(u => u.Persona)
-                .ThenInclude(p => p.DireccionesEnvio)
-                .FirstOrDefaultAsync(u =>
-                    (u.nombreUsuario == request.nombreUsuario || u.correo == request.correo));
+   if (usuario == null)
+   {
+    return BadRequest(new { message = "El token es inválido o ha expirado." });
+   }
 
-            if (usuario == null)
-            {
-                return Unauthorized(new { message = "Usuario o contraseña incorrectos" });
-            }
+   if (await IsPasswordUnsafe(request.NuevaContrasenia))
+   {
+    return BadRequest(new { message = "La nueva contraseña ingresada es insegura." });
+   }
 
-            // Verificar si el usuario está bloqueado
-            if (usuario.EstaBloqueado)
-            {
-                return Unauthorized(new { message = "Tu usuario está bloqueado. Restablece la contraseña para volver a ingresar." });
-            }
-            usuario.loginCount += 1;
-            await _context.SaveChangesAsync();
+   // Cambiar la contraseña y limpiar el token
+   usuario.contrasenia = BCrypt.Net.BCrypt.HashPassword(request.NuevaContrasenia);
+   usuario.ResetToken = null;
+   usuario.ResetTokenExpires = null;
 
-            // Verificar la contraseña
-            if (!BCrypt.Net.BCrypt.Verify(request.contrasenia, usuario.contrasenia))
-            {
-                usuario.IntentosFallidos++;
-                if (usuario.IntentosFallidos >= 3)
-                {
-                    usuario.EstaBloqueado = true;
+   // Verificar si el usuario está bloqueado y restablecer los intentos
+   if (usuario.EstaBloqueado)
+   {
+    usuario.IntentosFallidos = 0; // Restablecer intentos
+    usuario.EstaBloqueado = false; // Desbloquear al usuario
+   }
 
-                    // Generar el token de recuperación y su expiración
-                    var token = GenerateResetToken();
-                    usuario.ResetToken = token;
-                    usuario.ResetTokenExpires = DateTime.UtcNow.AddMinutes(15); // Expira en 15 minutos
+   await _context.SaveChangesAsync();
 
-                    await _context.SaveChangesAsync();
+   // Preparar el correo con el template de confirmación de cambio de contraseña
+   var body = System.IO.File.ReadAllText("Templates/PasswordUpdated.html")
+           .Replace("[LOGO_URL]", "https://i.imgur.com/EmvHFiH.png");
 
-                    // Enviar correo de notificación de bloqueo con el enlace para restablecer la contraseña
-                    var resetLink = $"http://localhost:4200/reset-password?token={token}";
-                    var body = System.IO.File.ReadAllText("Templates/BlockedAccount.html")
-                        .Replace("[LOGO_URL]", "https://i.imgur.com/EmvHFiH.png")
-                        .Replace("[RESET_LINK]", resetLink);
+   await emailService.SendEmailAsync(usuario.correo, "Tu contraseña ha sido actualizada", body);
 
-                    await emailService.SendEmailAsync(usuario.correo, "Cuenta bloqueada", body);
-
-                    return Unauthorized(new { message = "Tu cuenta ha sido bloqueada. Revisa tu correo para más detalles y restablece tu contraseña." });
-                }
-
-                await _context.SaveChangesAsync();
-                return Unauthorized(new { message = "Usuario o contraseña incorrectos" });
-            }
-
-            // Restablecer los intentos fallidos al iniciar sesión correctamente
-            usuario.IntentosFallidos = 0;
-            await _context.SaveChangesAsync();
-
-            // Obtener la zona horaria de León, Guanajuato (América/México_Ciudad)
-            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("America/Mexico_City");
-
-            // Convertir la hora UTC a la hora local de la zona horaria
-            var fechaInicioSesionLocal = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZoneInfo);
-
-            // Guardar un log de inicio de sesión con la hora local
-            var log = new LogInicioSesion
-            {
-                UsuarioId = usuario.idUsuario,
-                FechaInicioSesion = fechaInicioSesionLocal,
-                IpDireccion = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Desconocida"
-            };
-
-            await _context.LogInicioSesion.AddAsync(log);
-            await _context.SaveChangesAsync();
-
-            var response = new
-            {
-                message = "Inicio de sesión exitoso",
-                user = new
-                {
-                    usuario.idUsuario,
-                    usuario.nombreUsuario,
-                    usuario.correo,
-                    usuario.rol,
-                    usuario.type,
-                    persona = new
-                    {
-                        usuario.Persona?.Id,
-                        usuario.Persona?.Nombre,
-                        usuario.Persona?.Apellidos,
-                        usuario.Persona?.Telefono,
-                        usuario.Persona?.Correo,
-                        DireccionesEnvio = usuario.Persona?.DireccionesEnvio?.Select(d => new
-                        {
-                            d.Id,
-                            d.NombreDireccion,
-                            d.Calle,
-                            d.Numero,
-                            d.Colonia,
-                            d.Ciudad,
-                            d.Estado,
-                            d.CodigoPostal,
-                            d.EsPredeterminada
-                        }).ToList()
-                    }
-                }
-            };
-
-            return Ok(response);
-        }
-        [HttpGet("BuscarPorNombre")]
-        public async Task<ActionResult<IEnumerable<Usuario>>> SearchUsuariosPorNombre(string nombre)
-        {
-            if (string.IsNullOrEmpty(nombre))
-            {
-                return BadRequest("El nombre de usuario es requerido.");
-            }
-
-            var usuarios = await _context.Usuario
-                .Where(u => u.nombreUsuario.Contains(nombre))
-                .ToListAsync();
-
-            return Ok(usuarios); // Devuelve 200 OK con una lista (posiblemente vacía)
-        }
-
-        [HttpPost]
-        [Route("forgot-password")]
-        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request, [FromServices] EmailService emailService)
-        {
-            var usuario = await _context.Usuario.FirstOrDefaultAsync(u => u.correo == request.Correo);
-
-            if (usuario == null)
-            {
-                return BadRequest(new { message = "El correo no está registrado." });
-            }
-
-            // Verificar si ya hay un token activo y no ha expirado
-            if (usuario.ResetTokenExpires > DateTime.UtcNow)
-            {
-                return BadRequest(new { message = "Ya se ha iniciado un proceso de recuperación. Inténtalo más tarde." });
-            }
-
-            // Generar un nuevo token y establecer su fecha de expiración
-            var token = GenerateResetToken();
-            usuario.ResetToken = token;
-            usuario.ResetTokenExpires = DateTime.UtcNow.AddMinutes(5);
-
-            await _context.SaveChangesAsync();
-
-            // Preparar el correo con el template
-            var resetLink = $"http://localhost:4200/reset-password?token={token}";
-            var body = System.IO.File.ReadAllText("Templates/ForgotPassword.html")
-                       .Replace("[LOGO_URL]", "https://i.imgur.com/EmvHFiH.png")
-                       .Replace("[RESET_LINK]", resetLink);
-
-            await emailService.SendEmailAsync(usuario.correo, "Recuperación de contraseña", body);
-
-            return Ok(new { message = "Correo de recuperación enviado." });
-        }
-
-
-        private string GenerateResetToken()
-        {
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                var tokenData = new byte[32]; // Generar 32 bytes aleatorios
-                rng.GetBytes(tokenData);
-
-                // Convertir a Base64 y reemplazar caracteres problemáticos
-                return Convert.ToBase64String(tokenData)
-                              .Replace("+", "-")
-                              .Replace("/", "_")
-                              .Replace("=", ""); // Eliminar relleno
-            }
-        }
-
-        [HttpGet]
-        [Route("validate-token")]
-        public async Task<IActionResult> ValidateToken(string token)
-        {
-            var usuario = await _context.Usuario.FirstOrDefaultAsync(u => u.ResetToken == token && u.ResetTokenExpires > DateTime.UtcNow);
-            if (usuario == null)
-            {
-                return BadRequest(new { message = "El token es inválido o ha expirado." });
-            }
-
-            return Ok(true);
-        }
-
-        [HttpGet]
-        [Route("UltimoInicioSesion/{id:int}")]
-        public async Task<IActionResult> GetUltimoInicioSesion(int id)
-        {
-            // Verificar si el usuario existe
-            var usuario = await _context.Usuario.FindAsync(id);
-            if (usuario == null)
-            {
-                return NotFound(new { message = "Usuario no encontrado." });
-            }
-
-            // Obtener el segundo último inicio de sesión
-            var ultimoInicioSesion = await _context.LogInicioSesion
-                .Where(log => log.UsuarioId == id)
-                .OrderByDescending(log => log.FechaInicioSesion)
-                .Skip(1) // Saltar el inicio de sesión más reciente
-                .FirstOrDefaultAsync();
-
-            if (ultimoInicioSesion == null)
-            {
-                return NotFound(new { message = "No se encontraron registros de inicio de sesión previos." });
-            }
-
-            return Ok(new
-            {
-                FechaInicioSesion = ultimoInicioSesion.FechaInicioSesion,
-                IpDireccion = ultimoInicioSesion.IpDireccion
-            });
-        }
-
-        [HttpPost]
-        [Route("reset-password")]
-        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request, [FromServices] EmailService emailService)
-        {
-            var usuario = await _context.Usuario.FirstOrDefaultAsync(
-                u => u.ResetToken == request.Token && u.ResetTokenExpires > DateTime.UtcNow);
-
-            if (usuario == null)
-            {
-                return BadRequest(new { message = "El token es inválido o ha expirado." });
-            }
-
-            if (await IsPasswordUnsafe(request.NuevaContrasenia))
-            {
-                return BadRequest(new { message = "La nueva contraseña ingresada es insegura." });
-            }
-
-            // Cambiar la contraseña y limpiar el token
-            usuario.contrasenia = BCrypt.Net.BCrypt.HashPassword(request.NuevaContrasenia);
-            usuario.ResetToken = null;
-            usuario.ResetTokenExpires = null;
-
-            // Verificar si el usuario está bloqueado y restablecer los intentos
-            if (usuario.EstaBloqueado)
-            {
-                usuario.IntentosFallidos = 0; // Restablecer intentos
-                usuario.EstaBloqueado = false; // Desbloquear al usuario
-            }
-
-            await _context.SaveChangesAsync();
-
-            // Preparar el correo con el template de confirmación de cambio de contraseña
-            var body = System.IO.File.ReadAllText("Templates/PasswordUpdated.html")
-                    .Replace("[LOGO_URL]", "https://i.imgur.com/EmvHFiH.png");
-
-            await emailService.SendEmailAsync(usuario.correo, "Tu contraseña ha sido actualizada", body);
-
-            return Ok(new { message = "Contraseña restablecida con éxito." });
-        }
-    }
+   return Ok(new { message = "Contraseña restablecida con éxito." });
+  }
+ }
 
 
 }
