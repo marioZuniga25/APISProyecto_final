d550fc5f52abc28b33aec83923bd5f2e6d019d1b
Cristian Alejandro Leyva Collazo <cristianleyvacr7@gmail.com>
2024-11-27 10:47:03 -0600

    antes de fusionar

--- ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
+++ ProyectoFinalAPI/obj/Debug/net8.0/ProyectoFinalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305a2cf1c65a99b756e5a8ca14fac5e3088edd1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637e86966377a7bccb1a4dce2a9d439d3da02bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
